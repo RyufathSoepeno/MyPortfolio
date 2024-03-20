7978d6b316fd12b0d00ff3b2e45828eea9f42dc1
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-03-20 11:51:55 +0700

    Add files via upload

---
+++ Database Management/Movie Database with Oracle/droptables.sql
@@ -0,0 +1,14 @@
+-- ============================================================
+--   Database name:  ORACLE.CS.ARIZONA.EDU                                   
+--   DBMS name:      ORACLE Version 11g                          
+--   Modified on:    1/28/2013                       
+-- ============================================================
+
+drop table CASTING cascade constraints
+/
+
+drop table ACTOR cascade constraints
+/
+
+drop table MOVIE cascade constraints
+/
