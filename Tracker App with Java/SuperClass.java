636034acd43d441ad8669304e0cb8b1b1009bf3a
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-07-19 15:25:39 +0700

    Update SuperClass.java

--- Tracker App with Java/SuperClass.java
+++ Tracker App with Java/SuperClass.java
@@ -1,5 +1,9 @@
 /// This code explains "super" in Java
 
+/** The 'super' keyword allows referencing the parent class or superclass of a subclass in Java. 
+It is often employed to access members (fields or methods) of the superclass that have been overridden in the subclass. 
+You can call the superclass's method from within the subclass using super. methodName() */
+
 class SuperClass {
     SuperClass() {
         System.out.println("SuperClass constructor");
