a14bf72d3d74e3a35c5aeef295c974bfc7feecc4
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-08-27 11:18:14 +0700

    Update PatientInformation.php

--- Web Development Portfolio/Mentcare System/Mentcare System 3.0/PatientInformation.php
+++ Web Development Portfolio/Mentcare System/Mentcare System 3.0/PatientInformation.php
@@ -183,7 +183,7 @@
         // Database connection
         $servername = "localhost";
         $username = "root";
-        $password = "kkkkta";
+        $password = "";
         $dbname = "MentcareDB";
         $conn = new mysqli($servername, $username, $password, $dbname);
 
