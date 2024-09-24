ce5b01b53fd37aa43df3392e3de62b7434dcb809
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-09-24 12:12:34 +0700

    Create C. Constrained Layout.kt

---
+++ Mobile Development Portfolio/Android Studio Projects/C. Constrained Layout.kt
@@ -0,0 +1,13 @@
+package com.example.constraintlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
