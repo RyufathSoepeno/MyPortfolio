a5cc885e60b9e4885b31ae5c2ec08b19014c8587
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-10-01 10:29:21 +0700

    Create D. Image View.kt

---
+++ Mobile Development Portfolio/Android Studio Projects/D. Image View.kt
@@ -0,0 +1,8 @@
+ android:text="TextView"
+    app:layout_constraintStart_toStartOf="parent"
+    app:layout_constraintTop_toBottomOf="@+id/textView" />
+ and update android:text part into:
+ android:text="@string/sign_in_description"
+ 35. Result will appear on Design tab.
+ 36. Go to File and Save All
+ 37. Run on a generated AVD.
