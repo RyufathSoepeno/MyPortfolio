cc23dc98a1786f7b59d7ff4365232d0ce75dfab3
Ryufath Alief Adhyaksa Putera Soepeno <89006072+RyufathSoepeno@users.noreply.github.com>
2024-10-08 08:52:59 +0700

    Create H. activity_main.kt

---
+++ Mobile Development Portfolio/Android Studio Projects/H. activity_main.kt
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:id="@+id/linearLayout"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    app:layoutDescription="@xml/activity_main_scene">
+
+    <Spinner
+        android:id="@+id/spinner"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content" />
+</LinearLayout>
